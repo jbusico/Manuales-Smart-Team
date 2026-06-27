ab33c135fa2547bcd83603090819e557d4a392f8
Julián Búsico <busicojulian@gmail.com>
2026-06-27 15:11:31 -0300

    Primeros cambios

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME = 'ldtron-v1';
+
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
